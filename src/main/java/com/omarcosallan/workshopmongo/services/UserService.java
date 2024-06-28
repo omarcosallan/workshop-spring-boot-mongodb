6c0a58632605b8281e256c7f9a4d5e4cf4beaea5
Marcos <marcosallanm1to@gmail.com>
2024-06-28 10:31:45 -0300

    Inserção de usuário com POST

--- src/main/java/com/omarcosallan/workshopmongo/services/UserService.java
+++ src/main/java/com/omarcosallan/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.omarcosallan.workshopmongo.domain.User;
+import com.omarcosallan.workshopmongo.dto.UserDTO;
 import com.omarcosallan.workshopmongo.repository.UserRepository;
 import com.omarcosallan.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
